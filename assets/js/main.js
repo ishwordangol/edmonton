3ad659489f5d7b0082a5ffb6bc738f233620a6a4
ishwor <prataya2@gmail.com>
2024-08-28 20:40:23 +0545

    design update

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@ $(window).scroll(function () {
 $(".mainslider").slick({
     arrows: true,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 4000,
     dots: false,
     slidesToShow: 1,
     slidesToScroll: 1,
